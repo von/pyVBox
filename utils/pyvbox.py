6551e15f74439316addcc282a5504585b499cb49
Von Welch <von@vwelch.com>
2010-01-05 13:44:39 -0600

    Use Command classes for functions.

--- utils/pyvbox.py
+++ utils/pyvbox.py
@@ -38,132 +38,98 @@ def verboseMsg(msg):
 
 #----------------------------------------------------------------------
 #
-# Argument parsing functions
-#
-# These functions take some number of arguments from the commandline
-# (args) and return the resulting arguments to be appended to the
-# argument list to our command function.
-#
-# On error they should raise an exception.
+# Commands
 
-def parse_vm_filename(args):
-    """Parse one virtual machine by settings filename"""
-    try:
-        settingsFilename = args.pop(0)
-    except Exception, e:
-        raise Exception("missing virtual machine filename argument")
-    if not os.path.exists(settingsFilename):
-        raise Exception("settings file \"%s\" does not exist" %
-                        settingsFilename)
-    return VirtualMachine.open(settingsFilename)
+class Command:
+    """Base class for all commands."""
+    usage = "<command> <arguments"
 
-def parse_vm_name(args):
-    """Parse one virtual machine by name"""
-    try:
-        vmName = args.pop(0)
-    except Exception, e:
-        raise Exception("missing virtual machine argument")
-    return VirtualMachine.find(vmName)
+    @classmethod
+    def invoke(cls, args):
+        """Invoke the command.
 
-def parse_hdd_filenames(args):
-    """Parse zero or more remaining arguments as HDD filenames"""
-    disks = []
-    for filename in args:
-        disks.append(HardDisk.find(filename))
-    return disks
+        Should return exit code for program.
+        Should be overriden by child class."""
+        pass
 
-def parse_help_arg(args):
-    """Parse a help argument"""
-    try:
-        commandString = args.pop(0)
-    except Exception, e:
-        return None
-    commandArray = command_name_to_array(commandString)
-    return commandArray[0]
+    registered_commands = {}
 
-#----------------------------------------------------------------------
-#
-# Command functions
-#
-# These functions take parse command line arguments and perform some
-# function.
-#
-# On normal termination, their return value will be the exit code of
-# this program. On error they should raise an exception.
-#
-# Each function's docstring will be provided to the user as help.
+    @classmethod
+    def register_command(cls, name, command):
+        """Register the binding between name and command class"""
+        cls.registered_commands[name] = command
 
-def boot_vm(vm, *hdds):
-    """Boot the given virtual machine and eject it after power down.
+    @classmethod
+    def lookup_command_by_name(cls, name):
+        """Given a name, return associated registered command class"""
+        return cls.registered_commands[name]
 
-Usage: boot <vm config file> [<hd files>]"""
-    mode = "gui" # or "vrdp"
-    atexit.register(vm.eject)
-    if vm.registered():
-        verboseMsg("Machine \"%s\" already registered."% vm.getName())
-    else:
-        verboseMsg("Registering %s (%s)" % (vm.getName(), vm.getId()))
-        vm.register()
+class BootVMCommand(Command):
+    """Boot a virtual machine and eject it after power down"""
+    usage = "boot <VM settings file> [<HD files>]"
 
-    vm.openSession()
-    for hdd in hdds:
-        verboseMsg("Attaching HD \"%s\"" % hdd)
-        vm.attachDevice(hdd)
-    vm.closeSession()
+    @classmethod
+    def invoke(cls, args):
+        """Invoke the command. Return exit code for program."""
+        mode = "gui" # or "vrdp"
+        if len(args) < 1:
+            raise Exception("Missing virtual machine argument")
+        vm = VirtualMachine.open(args.pop(0))
+        atexit.register(vm.eject)
+        if not vm.registered():
+            vm.register()
+        vm.openSession()
+        for hd in args:
+            hd = HardDisk.find(hd)
+            vm.attachDevice(hd)
+        vm.closeSession()
+        verboseMsg("Starting VM in %s mode" % mode)
+        vm.openRemoteSession(type=mode)
+        # Wait until machine is running or we have a race condition
+        # where it still might be down when we call waitUntilDown()
+        vm.waitUntilRunning()
+        verboseMsg("VM started. Waiting until power down...")
+        vm.waitUntilDown()
+        verboseMsg("VM powered down. Cleaning up...")
+        vm.closeSession()
+        # Let atexit clean up
+        return 0
 
-    verboseMsg("Starting VM in %s mode" % mode)
-    vm.openRemoteSession(type=mode)
-    # Wait until machine is running or we have a race condition
-    # where it still might be down when we call waitUntilDown()
-    vm.waitUntilRunning()
+Command.register_command("boot", BootVMCommand)
+
+class EjectCommand(Command):
+    """Eject a virtual machine"""
     
-    verboseMsg("VM started. Waiting until power down...")
-    vm.waitUntilDown()
+    @classmethod
+    def invoke(cls, args):
+        """Invoke the command. Return exit code for program."""
+        if len(args) == 0:
+            raise Exception("Missing virtual machine name argument");
+        for name in args:
+            vm = VirtualMachine.find(name)
+            verboseMsg("Ejecting %s" % vm)
+            vm.eject()
+        return 0
 
-    verboseMsg("VM powered down. Cleaning up...")
-    vm.closeSession()
-    
-    # Let atexit clean up
-    return 0
+Command.register_command("eject", EjectCommand)
 
-def eject_vm(vm):
-    """Eject the given virtual machine.
+class HelpCommand(Command):
+    """Provide help"""
 
-Usage: eject <vm name>"""
-    message("Ejecting %s" % vm)
-    vm.eject()
-    return 0
+    @classmethod
+    def invoke(cls, args):
+        """Invoke the command. Return exit code for program."""
+        if len(args) > 0:
+            for name in args:
+                cmd = Command.lookup_command_by_name(name)
+                message("%10s: %s" % (name, cmd.__doc__))
+        else:
+            message("Available commands:")
+            for name in Command.registered_commands:
+                cmd = Command.lookup_command_by_name(name)
+                message("%10s: %s" % (name, cmd.__doc__))
 
-def help(function=None):
-    """Print help for the given command (or list command if no command given)
-
-Usage: help [<command>]"""
-    if function is None:
-        message("Available commands:")
-        for key in CommandMappings:
-            docString = CommandMappings[key][0].__doc__
-            helpString = docString.split("\n")[0]
-            message("%10s: %s" % (key, helpString))
-    else:
-        message(function.__doc__)
-
-# Mapping of command strings to command functions.  Key is the command
-# string given on the commandline.  First value of the array is the
-# command function to call.  Subsequent values in the array are
-# functions to be called in series to parse the commandline arguments.
-
-CommandMappings = {
-    "boot" : [ boot_vm, parse_vm_filename, parse_hdd_filenames ],
-    "eject" : [ eject_vm, parse_vm_name ],
-    "help" : [ help, parse_help_arg ],
-}
-
-def command_name_to_array(string):
-    """Map a command name to its associated array"""
-    if not CommandMappings.has_key(string):
-        raise Exception("unknown command \"%s\"" % string)
-
-    return CommandMappings[string]
+Command.register_command("help", HelpCommand)
 
 #----------------------------------------------------------------------
 
@@ -190,23 +156,9 @@ def main(argv=None):
         verbosityLevel = options.verbosityLevel
         verboseMsg("Setting verbosity level to %d" % verbosityLevel)
 
-    commandString = args.pop(0)
-    commandArray = command_name_to_array(commandString)
-    commandFunction = commandArray[0]
-    commandArgs = []
-    for argumentFunction in commandArray[1:]:
-        try:
-            newArgs = argumentFunction(args)
-            if type(newArgs) is list:
-                commandArgs.extend(newArgs)
-            else:
-                commandArgs.append(newArgs)
-        except Exception, e:
-            parser.error(str(e))
-    commandArgs.extend(args)
-
     try:
-        status = commandFunction(*commandArgs)
+        command = Command.lookup_command_by_name(args.pop(0))
+        status = command.invoke(args)
     except Exception, e:
         handle_exception(e)
         return 1
