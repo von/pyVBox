6952cdac5de945d20df2a1167ad35d0fe1aff602
Von Welch <von@vwelch.com>
2010-02-25 19:07:20 -0600

    StorageController now inherits Wrapper.

--- pyVBox/StorageController.py
+++ pyVBox/StorageController.py
@@ -1,13 +1,21 @@
 """Wrapper around IStorageController"""
 
-class StorageController:
+from Wrapper import Wrapper
+
+class StorageController(Wrapper):
+    # Properties directly inherited from IStorageController
+    _passthruProperties = [
+        "bus",
+        "controllerType",
+        "instance",
+        "maxDevicesPerPortCount",
+        "maxPortCount",
+        "minPortCount",
+        "name",
+        "portCount",
+        ]
 
     def __init__(self, storageController):
         """Return a StorageController around the given IStorageController instance"""
-        self._controller = storageController
-        
-    # Pass any requests for unrecognized attributes or methods onto
-    # IStorageController object. Doing this this way since I don't kow
-    # how to inherit the XPCOM object directly.
-    def __getattr__(self, attr):
-        return eval("self._controller." + attr)
+        assert(storageController is not None)
+        self._wrappedInstance = storageController
