20a81c70755eb882ffc9987469a08c717a1e125d
Von Welch <von@vwelch.com>
2010-04-08 08:32:30 -0500

    Revamped unittests so that we copy our test VM and HD first so we don't modify version controlled files.

--- test/pyVBoxTest.py
+++ test/pyVBoxTest.py
@@ -1,6 +1,8 @@
 """Base class for all pyVBox unittests."""
 
 import os
+import os.path
+import shutil
 import unittest
 
 from pyVBox.HardDisk import HardDisk
@@ -9,14 +11,32 @@ from pyVBox.VirtualMachine import VirtualMachine
 
 class pyVBoxTest(unittest.TestCase):
     """Base class for all pyVBox unittests."""
-    testHDpath = "test/appliances/TestHD.vdi"
-    cloneHDpath = "test/appliances/CloneHD.vdi"
-    bogusHDpath = "/bogus/path"
-    testVMpath = "test/appliances/TestVM.xml"
+    # VM and HD for testing
+    # These are version controlled, we will make a copy before
+    # altering them.
+    testHDsrc = "test/appliances/TestHD.vdi"
+    testVMsrc = "test/appliances/TestVM.xml"
     testVMname = "TestVM"
+
+    # Our testing grounds
+    testPath = "test/tmp/"
+    
+    # setUp() will create these copies of the sources above
+    testHDpath = "test/tmp/TestHD.vdi"
+    testVMpath = "test/tmp/TestVM.xml"
+
+    # HD clone we will create during testing
+    cloneHDpath = "test/tmp/CloneHD.vdi"
+
+    # Paths for testing failure
+    bogusHDpath = "/bogus/path"
     bogusVMpath = "/bogus/path"
 
     def setUp(self):
+        if not os.path.exists(self.testPath):
+            os.mkdir(self.testPath)
+        shutil.copy(self.testVMsrc, self.testVMpath)
+        shutil.copy(self.testHDsrc, self.testHDpath)
         self._cleanup()
 
     def tearDown(self):
