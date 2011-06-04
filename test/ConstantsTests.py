67d0ed3bfe968233e6d895c7a4df24ad3c412f75
Von Welch <von@vwelch.com>
2011-06-04 12:02:56 -0400

    Break about constants tests into categories. Modifications for new 4.0.8 constants.

--- test/ConstantsTests.py
+++ test/ConstantsTests.py
@@ -8,14 +8,39 @@ from pyVBox.VirtualBoxException import VirtualBoxException
 class ConstantsTests(pyVBoxTest):
     """Test case for Constants"""
 
-    def testConstants(self):
-        """Test VirtualBoxManger.Constants"""
-        s = Constants.SessionState_Closed
-        s = Constants.SessionState_Null
+    def testCleanupModeConstants(self):
+        """Test CleanupMode constants"""
+        s = Constants.CleanupMode_UnregisterOnly
+        s = Constants.CleanupMode_DetachAllReturnNone
+        s = Constants.CleanupMode_DetachAllReturnHardDisksOnly
+        s = Constants.CleanupMode_Full
+
+    def testDeviceTypeConstants(self):
+        """Test DeviceType constants"""
         s = Constants.DeviceType_HardDisk
+
+    def testMachineStateConstants(self):
+        """Test MachineState constants"""
         s = Constants.MachineState_Aborted
         s = Constants.MachineState_PoweredOff
         s = Constants.MachineState_Running
+
+    def testSessionStateConstants(self):
+        """Test SessionState constants"""
+        s = Constants.SessionState_Null
+        s = Constants.SessionState_Locked
+        s = Constants.SessionState_Unlocked
+        s = Constants.SessionState_Spawning
+        s = Constants.SessionState_Unlocking
+
+    def testSessionTypeConstants(self):
+        """Test SessionType constants"""
+        s = Constants.SessionType_Null
+        s = Constants.SessionType_WriteLock
+        s = Constants.SessionType_Remote
+        s = Constants.SessionType_Shared
+
+
 
 if __name__ == '__main__':
     main()
